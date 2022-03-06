3424a5c9783bc973eaeed26d3ef2322b671f8235
Nitro <66451996+N1-TR0@users.noreply.github.com>
2022-03-06 11:15:33 -0500

    Add files via upload

---
+++ Launcher.bat
@@ -0,0 +1,100 @@
+@ECHO off
+TITLE Lanceur De Demineur  
+mode con: cols= 30 lines=50
+color 0a 
+ECHO Chargement En Cours, Veuillez Patienter...
+TIMEOUT 10
+
+ECHO ====================================================================================================
+ECHO d8888b. d88888b .88b  d88. d888888b d8b   db d88888b db    db d8888b.      
+ECHO 88  `8D 88'     88'YbdP`88   `88'   888o  88 88'     88    88 88  `8D      
+ECHO 88   88 88ooooo 88  88  88    88    88V8o 88 88ooooo 88    88 88oobY'      
+ECHO 88   88 88~~~~~ 88  88  88    88    88 V8o88 88~~~~~ 88    88 88`8b        
+ECHO 88  .8D 88.     88  88  88   .88.   88  V888 88.     88b  d88 88 `88.      
+ECHO Y8888D' Y88888P YP  YP  YP Y888888P VP   V8P Y88888P ~Y8888P' 88   YD      
+ECHO ====================================================================================================
+ECHO Faite par CharlesD#9728 
+ECHO ====================================================================================================
+
+PAUSE 
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
+
+CD SRC
+"index.html"
+
+ECHO =====================================================
+ECHO  Terminé ! si la fenêtre du navigateur se ferme, appuyez sur Entrée 
+ECHO =====================================================
+
+PAUSE
